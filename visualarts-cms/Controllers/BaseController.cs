d21f12887780f61531c09e9b7767c8c1d33d4d8c
abgmgp <jfpapasin@gmail.com>
2025-03-05 07:05:18 +0800

    setup sql connection and base object

---
+++ visualarts-cms/Controllers/BaseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace visualarts_cms.Controllers
+{
+    public class BaseController : Controller
+    {
+        public static string connectionString = ConfigurationManager.ConnectionStrings["sqlConnection"].ConnectionString;
+        public SqlConnection conn = new SqlConnection(connectionString);
+    }
+}
