d2ada9bf8bc2138c381487be57bb4eadabf5b00a
abgmgp <jfpapasin@gmail.com>
2025-03-04 23:28:20 +0800

    add front end pages

--- visualarts-cms/Controllers/CurrentTrendsController.cs
+++ visualarts-cms/Controllers/CurrentTrendsController.cs
@@ -13,6 +13,11 @@ namespace visualarts_cms.Controllers
             return View();
         }
 
+        public ActionResult Read()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
