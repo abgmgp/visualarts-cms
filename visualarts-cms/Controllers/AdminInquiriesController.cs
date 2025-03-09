05a8605de4f45121ac319d00f6b3d86e57bfe6a4
abgmgp <jfpapasin@gmail.com>
2025-03-09 23:21:26 +0800

    admin interface template

---
+++ visualarts-cms/Controllers/AdminInquiriesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace visualarts_cms.Controllers
+{
+    public class AdminInquiriesController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult History()
+        {
+            return View();
+        }
+    }
+}
