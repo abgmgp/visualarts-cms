03f1fd4d71e8f78d89dcf3917b8c882769e5548b
abgmgp <jfpapasin@gmail.com>
2025-03-10 23:14:21 +0800

    add viewmodels

---
+++ visualarts-cms/Models/Viewmodel/UserAccountViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace visualarts_cms.Models.Viewmodel
+{
+    public class UserAccountViewModel : BaseViewModel
+    {
+        public int UserAccountId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
