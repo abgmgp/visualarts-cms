6b09da88690f51cbcbc3f9d21f3cc9f5458c8925
abgmgp <jfpapasin@gmail.com>
2025-03-14 00:10:29 +0800

    Add edit/view function for admin current trends

--- visualarts-cms/Controllers/AdminCurrentTrendsController.cs
+++ visualarts-cms/Controllers/AdminCurrentTrendsController.cs
@@ -39,9 +39,30 @@ namespace visualarts_cms.Controllers
 
         public ActionResult Restore()
         {
-            return View();
+            conn.Open();
+
+            var viewModel = new List<CurrentTrendViewModel>();
+            SqlCommand getQuery = new SqlCommand("SELECT * FROM CurrentTrends WHERE IsActive = 0", conn);
+            SqlDataReader reader = getQuery.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var ct = new CurrentTrendViewModel
+                {
+                    CurrentTrendId = Convert.ToInt32(reader["CurrentTrendId"]),
+                    Title = reader["Title"].ToString(),
+                    CreatedDateStr = Convert.ToDateTime(reader["DateCreated"]).ToString("MM/dd/yyyy"),
+                    UpdatedDateStr = !Convert.IsDBNull(reader["UpdatedDate"]) ? Convert.ToDateTime(reader["UpdatedDate"]).ToString("MM/dd/yyyy") : "",
+                    IsActive = Convert.ToBoolean(reader["IsActive"]),
+                };
+
+                viewModel.Add(ct);
+            }
+
+            return View(viewModel);
         }
 
+        #region create
         [HttpGet]
         public ActionResult Create()
         {
@@ -56,12 +77,12 @@ namespace visualarts_cms.Controllers
                 #region Check directories
                 if (!Directory.Exists(Server.MapPath("~/Content/Upload/Image")))
                 {
-                    Directory.CreateDirectory(Server.MapPath("~/Content/Upload/Image"));
+                    Directory.CreateDirectory(Server.MapPath("~/Content/Upload/Image/"));
                 }
 
-                if (!Directory.Exists(Server.MapPath("~/Content/Upload/Audio")))
+                if (!Directory.Exists(Server.MapPath("~/Content/Upload/Audio/")))
                 {
-                    Directory.CreateDirectory(Server.MapPath("~/Content/Upload/Audio"));
+                    Directory.CreateDirectory(Server.MapPath("~/Content/Upload/Audio/"));
                 }
                 #endregion
 
@@ -70,13 +91,13 @@ namespace visualarts_cms.Controllers
 
                 if (viewModel.ImageFile != null)
                 {
-                    var fullPathImage = Server.MapPath("~/Content/Upload/Image" + viewModel.ImageFile.FileName);
+                    var fullPathImage = Server.MapPath("~/Content/Upload/Image/" + viewModel.ImageFile.FileName);
                     viewModel.ImageFile.SaveAs(fullPathImage);
 
                 }
                 if (viewModel.AudioFile != null)
                 {
-                    var fullPathAudio = Server.MapPath("~/Content/Upload/Audio" + viewModel.AudioFile.FileName);
+                    var fullPathAudio = Server.MapPath("~/Content/Upload/Audio/" + viewModel.AudioFile.FileName);
                     viewModel.AudioFile.SaveAs(fullPathAudio);
                 }
                 #endregion
@@ -104,5 +125,117 @@ namespace visualarts_cms.Controllers
                 return RedirectToAction("Create");
             }
         }
+        #endregion create
+
+        #region edit
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            conn.Open();
+
+            var viewModel = new CurrentTrendViewModel();
+            SqlCommand getQuery = new SqlCommand("SELECT * FROM CurrentTrends WHERE CurrentTrendId = " + id, conn);
+            SqlDataReader reader = getQuery.ExecuteReader();
+
+            while (reader.Read())
+            {
+                viewModel = new CurrentTrendViewModel
+                {
+                    CurrentTrendId = Convert.ToInt32(reader["CurrentTrendId"]),
+                    Title = reader["Title"].ToString(),
+                    Content = reader["Content"].ToString(),
+                    ImagePath = reader["ImagePath"].ToString(),
+                    AudioPath = reader["AudioPath"].ToString(),
+                    EmbedUrl = reader["EmbedUrl"].ToString(),
+                };
+            }
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CurrentTrendViewModel viewModel)
+        {
+            try
+            {
+                #region Check directories
+                if (!Directory.Exists(Server.MapPath("~/Content/Upload/Image")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/Content/Upload/Image/"));
+                }
+
+                if (!Directory.Exists(Server.MapPath("~/Content/Upload/Audio/")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/Content/Upload/Audio/"));
+                }
+                #endregion
+
+                #region map full image path
+
+
+                if (viewModel.ImageFile != null)
+                {
+                    var fullPathImage = Server.MapPath("~/Content/Upload/Image/" + viewModel.ImageFile.FileName);
+                    viewModel.ImageFile.SaveAs(fullPathImage);
+
+                }
+                if (viewModel.AudioFile != null)
+                {
+                    var fullPathAudio = Server.MapPath("~/Content/Upload/Audio/" + viewModel.AudioFile.FileName);
+                    viewModel.AudioFile.SaveAs(fullPathAudio);
+                }
+                #endregion
+
+                //write to db
+                conn.Open();
+
+                SqlCommand insertQuery = new SqlCommand("INSERT INTO CurrentTrends " +
+                    "(Title, Content, EmbedUrl, AudioPath, ImagePath, IsActive, DateCreated) " +
+                    "VALUES (@Title, @Content, @EmbedUrl, @AudioPath, @ImagePath, @IsActive, @DateCreated)", conn);
+                insertQuery.Parameters.Add(new SqlParameter("@Title", viewModel.Title));
+                insertQuery.Parameters.Add(new SqlParameter("@Content", viewModel.Content));
+                insertQuery.Parameters.Add(new SqlParameter("@EmbedUrl", viewModel.EmbedUrl ?? ""));
+                insertQuery.Parameters.Add(new SqlParameter("@AudioPath", viewModel.AudioFile.FileName ?? ""));
+                insertQuery.Parameters.Add(new SqlParameter("@ImagePath", viewModel.ImageFile.FileName ?? ""));
+                insertQuery.Parameters.Add(new SqlParameter("@IsActive", true));
+                insertQuery.Parameters.Add(new SqlParameter("@DateCreated", DateTime.Now));
+
+                insertQuery.ExecuteNonQuery();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Create");
+            }
+        }
+        #endregion edit
+
+        #region view
+        [HttpGet]
+        public ActionResult View(int id)
+        {
+            conn.Open();
+
+            var viewModel = new CurrentTrendViewModel();
+            SqlCommand getQuery = new SqlCommand("SELECT * FROM CurrentTrends WHERE CurrentTrendId = " + id, conn);
+            SqlDataReader reader = getQuery.ExecuteReader();
+
+            while (reader.Read())
+            {
+                viewModel = new CurrentTrendViewModel
+                {
+                    CurrentTrendId = Convert.ToInt32(reader["CurrentTrendId"]),
+                    Title = reader["Title"].ToString(),
+                    Content = reader["Content"].ToString(),
+                    ImagePath = reader["ImagePath"].ToString(),
+                    AudioPath = reader["AudioPath"].ToString(),
+                    EmbedUrl = reader["EmbedUrl"].ToString(),
+                };
+            }
+
+            return View(viewModel);
+        }
+        #endregion view
     }
 }
