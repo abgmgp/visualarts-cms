bc35ce431e18ec5fc9bce6423fe01acbadae2845
abgmgp <jfpapasin@gmail.com>
2025-03-16 11:12:01 +0800

    admin dashboard

--- visualarts-cms/Controllers/AdminController.cs
+++ visualarts-cms/Controllers/AdminController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using visualarts_cms.Models.Viewmodel;
 
 namespace visualarts_cms.Controllers
 {
@@ -10,7 +12,50 @@ namespace visualarts_cms.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            conn.Open();
+            var viewmodel = new DashboardViewModel();
+
+            #region current trend count
+            SqlCommand getQueryCT = new SqlCommand("SELECT COUNT(CurrentTrendId) as Count FROM CurrentTrends WHERE IsActive = 1", conn);
+            SqlDataReader getQueryCTreader = getQueryCT.ExecuteReader();
+            while (getQueryCTreader.Read())
+            {
+                viewmodel.CurrentTrendCount = Convert.ToInt32(getQueryCTreader["Count"]);
+            }
+            #endregion
+
+            #region inquiry count
+            SqlCommand getQueryInq = new SqlCommand("SELECT COUNT(InquiryId) as Count FROM Inquiries WHERE IsActive = 1", conn);
+            SqlDataReader getQueryInqReader = getQueryInq.ExecuteReader();
+            while (getQueryInqReader.Read())
+            {
+                viewmodel.InquiryCount = Convert.ToInt32(getQueryInqReader["Count"]);
+            }
+            #endregion
+
+            #region trend list
+            var currentTrends = new List<CurrentTrendViewModel>();
+            SqlCommand getQueryCTList = new SqlCommand("SELECT * FROM CurrentTrends WHERE IsActive = 1", conn);
+            SqlDataReader getQueryCTListreader = getQueryCTList.ExecuteReader();
+            while (getQueryCTListreader.Read())
+            {
+                var ct = new CurrentTrendViewModel
+                {
+                    CurrentTrendId = Convert.ToInt32(getQueryCTListreader["CurrentTrendId"]),
+                    Title = getQueryCTListreader["Title"].ToString(),
+                    CreatedDateStr = Convert.ToDateTime(getQueryCTListreader["DateCreated"]).ToString("MM/dd/yyyy"),
+                    UpdatedDateStr = !Convert.IsDBNull(getQueryCTListreader["UpdatedDate"]) ? Convert.ToDateTime(getQueryCTListreader["UpdatedDate"]).ToString("MM/dd/yyyy") : "",
+                    IsActive = Convert.ToBoolean(getQueryCTListreader["IsActive"]),
+                    ImagePath = getQueryCTListreader["ImagePath"].ToString(),
+                };
+
+                currentTrends.Add(ct);
+            }
+            viewmodel.CurrentTrendList = currentTrends;
+            #endregion
+
+
+            return View(viewmodel);
         }
     }
 }
---
+++ visualarts-cms/Models/Viewmodel/DashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace visualarts_cms.Models.Viewmodel
+{
+    public class DashboardViewModel
+    {
+        public int CurrentTrendCount { get; set; }
+        public int InquiryCount { get; set; }
+        public List<CurrentTrendViewModel> CurrentTrendList { get;set; }
+    }
+}
